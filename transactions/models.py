cfda2dcd97ebbfeed6e1e6f160b22354b6d66e99
Carlos Reyes <andrespk3000@gmail.com>
2025-02-19 23:52:18 -0500

    backend set up --workingOn front

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Transaction(models.Model):
+
+    choices = (
+        ('expense', 'expense'),
+        ('income', 'income')
+    )
+
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=100, choices=choices)
+
+    def __str__(self):
+        return f'{self.title} - {self.amount}  - {self.category} '
--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Transaction
+from .serializers import TransactionSerializer
+
 
 # Create your views here.
+
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    serializer_class = TransactionSerializer
+    queryset = Transaction.objects.all()
