222e80e7b4308dd2facc6e7fdcf3427712a1d6dd
Carlos Reyes <andrespk3000@gmail.com>
2025-02-21 00:19:09 -0500

    react navigation done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import TransactionsFormPage from "./pages/TransactionFormPage";
+import TransactionsPage from "./pages/TransactionsPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<TransactionsPage />} />
+          <Route path="/create/" element={<TransactionsFormPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/pages/TransactionFormPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TransactionFormPage = () => {
+  return <div>TransactionFormPage</div>;
+};
+
+export default TransactionFormPage;
